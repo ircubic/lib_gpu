e7add10cb0d8da5a9cad30c00eeb87d40dce7908
Daniel Bruce <ircubic@gmail.com>
2016-04-11 11:55:47 +0200

    Add a poll rate limit.

--- lib_gpu/nvidia_simple_api.cpp
+++ lib_gpu/nvidia_simple_api.cpp
@@ -4,11 +4,17 @@
 #include "nvidia_interface.h"
 
 static std::unique_ptr<NvidiaApi> api{};
+static ULONGLONG last_poll = 0;
+
+// To avoid unnecessary polling with the simple API, we enforce a max pollrate
+const int MAX_POLLS_PER_SEC = 4;
+const ULONGLONG MIN_POLL_INTERVAL = 1000 / MAX_POLLS_PER_SEC;
 
 bool ensureApi() 
 {
     if (!api) {
         api.reset(new NvidiaApi());
+        last_poll = 0;
         if (api->getGPUCount() <= 0) {
             api.reset(nullptr);
         }
@@ -20,7 +26,13 @@ std::shared_ptr<NvidiaGPU> getUpdatedGPU(int num = 0) {
     if (ensureApi()) {
         auto gpu = api->getGPU(num);
         if (gpu) {
-            gpu->poll();
+            ULONGLONG now = GetTickCount64();
+
+            if (now - last_poll > MIN_POLL_INTERVAL) {
+                gpu->poll();
+                last_poll = now;
+            }
+
             return gpu;
         }
     }
@@ -71,6 +83,9 @@ bool overclock(float new_delta, int area)
         GpuOverclockDefinitionMap map;
         map[(GPU_OVERCLOCK_SETTING_AREA)area] = new_delta;
         success = gpu->setOverclock(map);
+
+        // We force a poll just to ensure the overclock profile is correct
+        gpu->poll();
     }
     return success;
 }
