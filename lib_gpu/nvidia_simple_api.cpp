8a8ddacaa7319b8cf7fc22ea117edd3f1efb0d11
Daniel Bruce <ircubic@gmail.com>
2016-04-11 14:55:25 +0200

    Allow poll time per-gpu

--- lib_gpu/nvidia_simple_api.cpp
+++ lib_gpu/nvidia_simple_api.cpp
@@ -2,9 +2,11 @@
 #include "nvidia_simple_api.h"
 #include "lib_gpu_nvidia.h"
 #include "nvidia_interface.h"
+#include <mutex>
 
 static std::unique_ptr<NvidiaApi> api{};
-static ULONGLONG last_poll = 0;
+static std::vector<ULONGLONG> last_poll;
+static std::mutex api_mutex;
 
 // To avoid unnecessary polling with the simple API, we enforce a max pollrate
 const int MAX_POLLS_PER_SEC = 4;
@@ -12,11 +14,15 @@ const ULONGLONG MIN_POLL_INTERVAL = 1000 / MAX_POLLS_PER_SEC;
 
 bool ensureApi() 
 {
+    std::lock_guard<std::mutex> lock(api_mutex);
     if (!api) {
         api.reset(new NvidiaApi());
-        last_poll = 0;
-        if (api->getGPUCount() <= 0) {
+        last_poll.clear();
+        auto count = api->getGPUCount();
+        if (count <= 0) {
             api.reset(nullptr);
+        } else {
+            last_poll.resize(count, 0);
         }
     }
     return (bool)api;
@@ -24,13 +30,14 @@ bool ensureApi()
 
 std::shared_ptr<NvidiaGPU> getUpdatedGPU(unsigned int num = 0) {
     if (ensureApi()) {
+        std::lock_guard<std::mutex> lock(api_mutex);
         auto gpu = api->getGPU(num);
         if (gpu) {
             ULONGLONG now = GetTickCount64();
-
-            if (now - last_poll > MIN_POLL_INTERVAL) {
+            auto last = last_poll[num];
+            if (now - last > MIN_POLL_INTERVAL) {
                 gpu->poll();
-                last_poll = now;
+                last_poll[num] = now;
             }
 
             return gpu;
